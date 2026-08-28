77827fd11f10e3611f4de87281b8bd8ec3a3f9c2
hihei56 <95565742+hihei56@users.noreply.github.com>
2026-08-28 18:57:58 +0900

    refactor: Update client.js to accept config parameter for multi-account support

--- src/client.js
+++ src/client.js
@@ -1,10 +1,21 @@
 const { Client } = require('discord.js-selfbot-v13');
 
-function createClient() {
+/**
+ * Discord クライアントを生成
+ * @param {Object} config - 設定オブジェクト（オプション、複数アカウント対応）
+ * @returns {Client} discord.js-selfbot-v13 クライアント
+ */
+function createClient(config) {
   return new Client({
     checkUpdate: false,
     syncStatus: true,
-    ws: { properties: { $os: 'Windows', $browser: 'Discord Client', $device: 'Discord Client' } }
+    ws: {
+      properties: {
+        $os: 'Windows',
+        $browser: 'Discord Client',
+        $device: 'Discord Client'
+      }
+    }
   });
 }
 
